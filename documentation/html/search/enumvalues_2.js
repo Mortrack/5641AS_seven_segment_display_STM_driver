0d910e6b1d002540547f89e71052e03d56e45967
Mortrack <cmirandameza3@hotmail.com>
2024-01-31 19:46:35 -0800

    I have updated the offline Doxygen documentation of this Library with respect to the latest change.

--- documentation/html/search/enumvalues_2.js
+++ documentation/html/search/enumvalues_2.js
@@ -14,8 +14,8 @@ var searchData=
   ['letter_5fequals_5fsign_5fin_5fascii_11',['Letter_equals_sign_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3ad1485d2a7b89b203ae670365f0275d62',1,'5641as_display_driver.c']]],
   ['letter_5ff_5fin_5fascii_12',['Letter_f_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a5bf75335958686375c872dc0c42d58ee',1,'5641as_display_driver.c']]],
   ['letter_5ff_5fin_5fascii_13',['Letter_F_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a7fa548d2e5651e113d48b7b0eb84a02b',1,'5641as_display_driver.c']]],
-  ['letter_5fh_5fin_5fascii_14',['Letter_h_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a8f2fe0a9346953a855ca1b7861244762',1,'5641as_display_driver.c']]],
-  ['letter_5fh_5fin_5fascii_15',['Letter_H_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3ae7be9d8969815128ef5aba356a93202a',1,'5641as_display_driver.c']]],
+  ['letter_5fh_5fin_5fascii_14',['Letter_H_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3ae7be9d8969815128ef5aba356a93202a',1,'5641as_display_driver.c']]],
+  ['letter_5fh_5fin_5fascii_15',['Letter_h_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a8f2fe0a9346953a855ca1b7861244762',1,'5641as_display_driver.c']]],
   ['letter_5fi_5fin_5fascii_16',['Letter_i_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3aeb58e6b5dcac25954b6bf5a5d0d80ff9',1,'5641as_display_driver.c']]],
   ['letter_5fl_5fin_5fascii_17',['Letter_L_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a74e8c643110be7f71e46a90352e5fe4d',1,'5641as_display_driver.c']]],
   ['letter_5fl_5fin_5fascii_18',['Letter_l_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a3d743770a67fa1b9688bc51d805e09e0',1,'5641as_display_driver.c']]],
@@ -28,8 +28,9 @@ var searchData=
   ['letter_5fr_5fin_5fascii_25',['Letter_r_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a8eede994636442818b3e6d66b398f066',1,'5641as_display_driver.c']]],
   ['letter_5fs_5fin_5fascii_26',['Letter_S_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a835a0b4d9d7ec69964e3b6a869633a31',1,'5641as_display_driver.c']]],
   ['letter_5fsingle_5fquotation_5fmark_5fin_5fascii_27',['Letter_single_quotation_mark_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3acca775b3779a5f34efee421b8b7e7b38',1,'5641as_display_driver.c']]],
-  ['letter_5fu_5fin_5fascii_28',['Letter_U_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a3a0445e64499761b456d70af7a386c0c',1,'5641as_display_driver.c']]],
+  ['letter_5ft_5fin_5fascii_28',['Letter_t_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a8691319867fb49e9a1f147b9281ce5ba',1,'5641as_display_driver.c']]],
   ['letter_5fu_5fin_5fascii_29',['Letter_u_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3af09a51edc64048ce847cf8d9d2f4c4a8',1,'5641as_display_driver.c']]],
-  ['letter_5funderscore_5fin_5fascii_30',['Letter_underscore_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3af2efce5f6d61107072f4f8df9b0c0212',1,'5641as_display_driver.c']]],
-  ['letter_5fvertical_5fbar_5fin_5fascii_31',['Letter_vertical_bar_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3ac232498d03815b13a8f709ead6f604ee',1,'5641as_display_driver.c']]]
+  ['letter_5fu_5fin_5fascii_30',['Letter_U_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3a3a0445e64499761b456d70af7a386c0c',1,'5641as_display_driver.c']]],
+  ['letter_5funderscore_5fin_5fascii_31',['Letter_underscore_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3af2efce5f6d61107072f4f8df9b0c0212',1,'5641as_display_driver.c']]],
+  ['letter_5fvertical_5fbar_5fin_5fascii_32',['Letter_vertical_bar_in_ASCII',['../df/d0a/group__display__5641as.html#gga81cf8a56aceee8d000b88730d17ceaa3ac232498d03815b13a8f709ead6f604ee',1,'5641as_display_driver.c']]]
 ];
