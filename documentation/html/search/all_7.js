30ba37f1c0b8691329bb7f4e63b331ca922559e6
Mortrack <cmirandameza3@hotmail.com>
2024-02-03 11:37:42 -0800

    I updated the offline Doxygen documentation of the project with respect to what was added in the latest release.

--- documentation/html/search/all_7.js
+++ documentation/html/search/all_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['g_0',['G',['../da/d45/struct_display__5641_a_s__peripherals__def__t.html#afacce5c07fb9b6190fbad2be700b1028',1,'Display_5641AS_peripherals_def_t']]],
-  ['get_5f5641as_5fdisplay_5foutput_1',['get_5641as_display_output',['../df/d0a/group__display__5641as.html#ga6dcf5e3de1d23609379f787437919b05',1,'5641as_display_driver.c']]],
+  ['get_5f5641as_5fdisplay_5foutput_1',['get_5641as_display_output',['../df/d0a/group__display__5641as.html#ga56fe567f14d6b44fc2acc7046cc1954b',1,'5641as_display_driver.c']]],
   ['get_5fdisplay_5foff_5ftime_5fsteps_2',['get_display_off_time_steps',['../df/d0a/group__display__5641as.html#gadebbbbac28f5b979c0c1bc19c19e6f20',1,'5641as_display_driver.c']]],
   ['get_5fdisplay_5fon_5ftime_5fsteps_3',['get_display_on_time_steps',['../df/d0a/group__display__5641as.html#gadd960afdf4d0758b306b2f9596a984bd',1,'5641as_display_driver.c']]],
   ['gpio_5fpin_4',['GPIO_Pin',['../d1/d9c/struct_display__5641_a_s___g_p_i_o__def__t.html#a54d7f52d3553cc2914237797fe64b28f',1,'Display_5641AS_GPIO_def_t']]],
--- documentation/html/search/functions_3.js
+++ documentation/html/search/functions_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['set_5f5641as_5fdisplay_5foutput_0',['set_5641as_display_output',['../df/d0a/group__display__5641as.html#gaa54a076df96146323435200d9eec7e91',1,'5641as_display_driver.c']]],
+  ['set_5f5641as_5fdisplay_5foutput_0',['set_5641as_display_output',['../df/d0a/group__display__5641as.html#gae1d706a7aeace1c0da7eee8a0a9d412c',1,'5641as_display_driver.c']]],
   ['set_5fdisplay_5foff_5ftime_5fsteps_1',['set_display_off_time_steps',['../df/d0a/group__display__5641as.html#ga178fc1d0d635712da5a324ee2415a6de',1,'5641as_display_driver.c']]],
   ['set_5fdisplay_5fon_5ftime_5fsteps_2',['set_display_on_time_steps',['../df/d0a/group__display__5641as.html#ga0444ff146582690e08346ff006a94873',1,'5641as_display_driver.c']]]
 ];
